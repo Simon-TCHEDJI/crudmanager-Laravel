31156b2c7a4ab5ab9b05e87ae995b2ea7fc4b485
Simon-TCHEDJI <tchedji.simonck@gmail.com>
2023-10-02 10:45:04 +0100

    route /login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view("pages.connexion");
 });
+
+Route::get('/login', function () {
+    return view("pages.connexion");
+});
